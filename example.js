46db8f30896ec83fd4a315d5f891f6741bfb5da1
unknown <pererva.kostya@outlook.com>
2020-12-22 01:13:45 +0200

    new css

--- example.js
+++ example.js
@@ -4,27 +4,49 @@ const fs = require('fs');
 // const iPhone = puppeteer.devices['iPhone 6'];
 
 const sitePage = [
-    'https://avtodiski.net.ua',                                                                    //Главная
-    'https://avtodiski.net.ua/product/bolty',                                                      //Каталог товара
-    'https://avtodiski.net.ua/product/bolty/bolt-chrome-m12-125-l27.html',                         //Детальная товара
-    'https://avtodiski.net.ua/otzyvy',                                                             //Отзывы
-    'https://avtodiski.net.ua/stati',                                                              //Статьи
-    'https://avtodiski.net.ua/stati/vazhno-znat',                                                  //Детальная статьи
-    'https://avtodiski.net.ua/calc',                                                               //Шинный калькулятор
-    'https://avtodiski.net.ua/novosti',                                                            //Новости
-    'https://avtodiski.net.ua/novosti/diski-original-komplekt-shin',                               //Детальная новости
-    'https://avtodiski.net.ua/shina',                                                              //Каталог шина
-    'https://avtodiski.net.ua/disk/zw/355-21024',                                                  //Детальная диски
-    'https://avtodiski.net.ua/disk/avto',                                                          //Подбор дисков по авто
-    'https://avtodiski.net.ua/disk/avto/geely',                                                    //Подбор дисков по авто - выбор машины
-    'https://avtodiski.net.ua/disk/diametr-14/shirina-5_6/pcd-4x100?diametr=15&et=30_45&dia=56-6', //Фильтр дисков по выбранной машине
-    'https://avtodiski.net.ua/page/garantii',                                                      //Гарантии
-    'https://avtodiski.net.ua/page/dostavka-oplata',                                               //Доставка и оплата
-    'https://avtodiski.net.ua/shina?ac=on&ex=on',                                                  // Акции шины
-    'https://avtodiski.net.ua/page/kontakty',                                                      //Контакты
+    { link:'https://avtodiski.net.ua',
+        nameFile: 'home'},                                                               //Главная
+    { link:'https://avtodiski.net.ua/product/bolty',
+        nameFile: 'catalog-goods'},                                                      //Каталог товара
+    { link:'https://avtodiski.net.ua/product/bolty/bolt-chrome-m12-125-l27.html',
+        nameFile: 'detail-goods'},                                                       //Детальная товара
+    { link:'https://avtodiski.net.ua/otzyvy',
+        nameFile: 'reviews'},                                                            //Отзывы
+    { link:'https://avtodiski.net.ua/stati',
+        nameFile: 'articles'},                                                           //Статьи
+    { link:'https://avtodiski.net.ua/stati/vazhno-znat',
+        nameFile: 'detail-articles'},                                                    //Детальная статьи
+    { link:'https://avtodiski.net.ua/calc',
+        nameFile: 'calc'},                                                               //Шинный калькулятор
+    { link:'https://avtodiski.net.ua/novosti',
+        nameFile: 'news'},                                                               //Новости
+    { link:'https://avtodiski.net.ua/novosti/diski-original-komplekt-shin',
+        nameFile: 'detail-news'},                                                        //Детальная новости
+    { link:'https://avtodiski.net.ua/shina',
+        nameFile: 'catalog-shina'},                                                      //Каталог шина
+    { link:'https://avtodiski.net.ua/disk/zw/355-21024',
+        nameFile: 'detail-disk'},                                                        //Детальная диски
+    { link:'https://avtodiski.net.ua/disk/avto',
+        nameFile: 'select-avto'},                                                        //Подбор дисков по авто
+    { link:'https://avtodiski.net.ua/disk/avto/geely',
+        nameFile: 'filter-car'},                                                         //Подбор дисков по авто - выбор машины
+    { link:'https://avtodiski.net.ua/disk/diametr-14/shirina-5_6/pcd-4x100?diametr=15&et=30_45&dia=56-6',
+        nameFile: 'catalog-filter-car'},                                                 //Фильтр дисков по выбранной машине
+    { link:'https://avtodiski.net.ua/page/garantii',
+        nameFile: 'guaranty'},                                                          //Гарантии
+    { link:'https://avtodiski.net.ua/page/dostavka-oplata',
+        nameFile: 'delivery'},                                                          //Доставка и оплата
+    { link:'https://avtodiski.net.ua/shina?ac=on&ex=on',
+        nameFile: 'save-shina'},                                                        // Акции шины
+    { link:'https://avtodiski.net.ua/page/kontakty',
+        nameFile: 'contacts'},                                                          //Контакты
+];
+const sitePage1 = [
+    { link:'https://avtodiski.net.ua/disk/zw/355-21024',
+        nameFile: 'detail-disk'},
 ];
 
-let counter = 0;
+let counter = 1;
 
 function pup(pageLink) {
     if (counter > sitePage.length) return;
@@ -43,11 +65,11 @@ function pup(pageLink) {
         ]);
 
         // Visit desired page
-        await page.goto(pageLink);
+        await page.goto(pageLink.link);
 
         const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-        await wait(120000);
+        await wait(360000);
 
         //Stop collection and retrieve the coverage iterator
         const cssCoverage = await Promise.all([
@@ -83,18 +105,18 @@ function pup(pageLink) {
 
 
 
-        fs.writeFile("./exported_css" + counter + ".css", covered_css, function(err) {
+        fs.writeFile("./css/" + pageLink.nameFile + ".css", covered_css, function(err) {
             if(err) {
                 return console.log(err);
             }
-            console.log("The file was saved!");
+            console.log("file:" + pageLink.nameFile + ".css");
         });
 
         await browser.close();
 
-        counter++;
-
         pup(sitePage[counter]);
+
+        counter++;
 
     })(pageLink);
 }
